204dcd6a771bfa184097004a309754c1b782d813
simdeistud <simone.deiana@studenti.units.it>
2024-04-02 12:18:17 +0200

    Added a Register class to model a single Register.

---
+++ LEGv8_Simulator/src/main/java/com/arm/legv8simulator/client/cpu/Register.java
@@ -0,0 +1,44 @@
+package com.arm.legv8simulator.client.cpu;
+
+import com.arm.legv8simulator.client.memory.Memory;
+
+public class Register {
+	
+	private final RegisterType type; // maybe useless
+	private final int size;
+	private long content;
+	
+	public Register(RegisterType type) {
+		
+		this.type = type;
+		
+		switch(type) {
+		case X:
+		case D:		this.size = Memory.DOUBLEWORD_SIZE;
+					break;
+		case S:		this.size = Memory.WORD_SIZE;
+					break;
+		default:	this.size = Memory.DOUBLEWORD_SIZE;
+		}
+		
+		content = 0L;		// S registers are just the lower part of the D registers
+	}
+	
+	public void writeDoubleWord(long bits) {
+		this.content = bits;
+	}
+	
+	public long readDoubleWord() {
+		return this.content;
+	}
+	
+	public void writeWord(int bits) {
+		this.content = (this.content & 0xffffffff00000000L) | (bits & 0x00000000ffffffffL);
+	}
+	
+	public int readWord() {
+		return (int) this.content;
+	}
+
+
+}
