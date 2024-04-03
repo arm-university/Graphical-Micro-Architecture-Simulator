2edf5ad52c7c1407b0b3bf7ef5c976d3b48917c4
simdeistud <simone.deiana@studenti.units.it>
2024-04-03 10:19:26 +0200

    Fixed block in pipeline execution when reaching last instruction

--- LEGv8_Simulator/src/main/java/com/arm/legv8simulator/client/executionmodes/PipelinedSimulator.java
+++ LEGv8_Simulator/src/main/java/com/arm/legv8simulator/client/executionmodes/PipelinedSimulator.java
@@ -20,6 +20,88 @@ import com.arm.legv8simulator.client.lexer.TokenType;
  */
 public class PipelinedSimulator extends LEGv8_Simulator {
 	
+		// All of the unused variables are control signal values - can be obtained from ControlUnitConfigureations class
+		// will be necessary when visualising the pipeline
+		
+		// It could be argued strongly that each pipeline register should be represented by an 
+		// object to make this a cleaner, more hierarchical structure!
+		
+		// values stored in IFID pipeline register
+		private long IFID_PC4 = -1;
+		private long IFID_Instruction = -1;
+		private int IFID_Rn = -1;
+		private int IFID_Rm = -1;
+		private int IFID_Rd = -1;
+		
+		// values stored in IDEX pipeline register
+		private boolean XIDEX_WB_RegWrite = false;
+		private boolean XIDEX_WB_MemToReg = false;
+		private boolean XIDEX_M_MemRead = false;
+		private boolean XIDEX_M_MemWrite = false;
+		private Integer XIDEX_EX_ALUOp = null;
+		private boolean XIDEX_EX_ALUSrc = false;
+		private Long XIDEX_ReadData1 = null;
+		private Long XIDEX_ReadData2 = null;
+		private int XIDEX_Rn = -1;
+		private int XIDEX_Rm = -1;
+		private int XIDEX_Rd = -1;
+		
+		/*private boolean DIDEX_WB_RegWrite = false;
+		private boolean DIDEX_WB_MemToReg = false;
+		private boolean DIDEX_M_MemRead = false;
+		private boolean DIDEX_M_MemWrite = false;
+		private Integer DIDEX_EX_ALUOp = null;
+		private boolean DIDEX_EX_ALUSrc = false;
+		private Double DIDEX_ReadData1 = null;
+		private Double DIDEX_ReadData2 = null;
+		private int DIDEX_Rn = -1;
+		private int DIDEX_Rm = -1;
+		private int DIDEX_Rd = -1;
+		
+		private boolean SIDEX_WB_RegWrite = false;
+		private boolean SIDEX_WB_MemToReg = false;
+		private boolean SIDEX_M_MemRead = false;
+		private boolean SIDEX_M_MemWrite = false;
+		private Integer SIDEX_EX_ALUOp = null;
+		private boolean SIDEX_EX_ALUSrc = false;
+		private Float SIDEX_ReadData1 = null;
+		private Float SIDEX_ReadData2 = null;
+		private int SIDEX_Rn = -1;
+		private int SIDEX_Rm = -1;
+		private int SIDEX_Rd = -1;*/
+		
+		// values stored in the EXMEM pipeline register
+		private boolean EXMEM_WB_RegWrite = false;
+		private boolean EXMEM_WB_MemToReg = false;
+		private boolean EXMEM_M_MemRead =  false;
+		private boolean EXMEM_M_MemWrite = false;
+		private Long EXMEM_ALUResult = null;
+		private Long EXMEM_WriteDataMem = null;
+		private int EXMEM_Rd = -1;
+		
+		// values stored in the MEMWB pipeline refister
+		private boolean MEMWB_WB_RegWrite = false;
+		private boolean MEMWB_WB_MemToReg = false;
+		private Long MEMWB_ReadDataMem = null;
+		private Long MEMWB_ALUResult = null;
+		private int MEMWB_Rd = -1;
+		
+		// data hazard variables
+		private boolean dataHazardStall = false;
+		private boolean branchDataHazardStall = false;
+		private boolean EXHazard = false;
+		private boolean MEMHazard = false;
+		private boolean forwardA = false;
+		private boolean forwardB = false;
+		//control hazard variables
+		private boolean controlHazardStall = false;
+		private Instruction nextInstruction;
+		
+		private StringBuilder pipelineLog = new StringBuilder("");
+		private CPUSnapshot visibleState;
+		// array representing the pipeline
+		private PipelineInstruction[] pipeline = new PipelineInstruction[PIPELINE_SIZE];
+	
 	public static final int PIPELINE_SIZE = 5;
 
 	/**
@@ -36,15 +118,18 @@ public class PipelinedSimulator extends LEGv8_Simulator {
 	public void clock() {
 		if (cpu.getInstructionIndex() == cpuInstructions.size()) {
 			// if no more new instructions to insert into pipeline execute those left in pipeline; or if empty, do nothing
+			pipelineLog.append("We are at the last instruction \n");
 			currentLineNumber = cpuInstructions.get(cpuInstructions.size()-1).getLineNumber();
-			if (!pipelineEmpty()) {
+			if (!isPipelineEmpty()) {
+				pipelineLog.append("The pipeline is not yet empty \n");
 				if (dataHazardStallRequired()) {
 					insertBubble(2);
 				} else {
+					pipelineLog.append("Moving the pipeline along \n");
 					// insert bubble at IF position - no more instructions to insert
 					updatePipeline(null);
 				}
-			}
+			} else 	pipelineLog.append("Execution has ended \n");
 		} else {
 			// come here if still new instructions to enter pipeline
 			if (dataHazardStallRequired()) {
@@ -82,20 +167,19 @@ public class PipelinedSimulator extends LEGv8_Simulator {
 		detectDataHazard();
 	}
 	
-	// tru if there is a data hazard present
+	// true if there is a data hazard present
 	private boolean dataHazardStallRequired() {
 		return dataHazardStall || branchDataHazardStall;
 	}
 	
 	// returns true if pipeline is empty
-	private boolean pipelineEmpty() {
-		boolean empty = true;
-		for (int i=0; i<PIPELINE_SIZE; i++) {
-			if (pipeline[i] != null) {
-				empty = false;
-			} 
+	private boolean isPipelineEmpty() {
+		int nonEmptyCounter = 0;
+		for(PipelineInstruction pi : pipeline) {
+			if(pi != null)
+				nonEmptyCounter++;
 		}
-		return empty;
+		return nonEmptyCounter == 0;
 	}
 	
 	// moves each instruction along one position in the pipeline and inserts the next instruction to the IF stage
@@ -207,7 +291,7 @@ public class PipelinedSimulator extends LEGv8_Simulator {
 		boolean hazardPresent = false;
 		if (pipeline[1] != null) {
 			if (pipeline[1].getInstruction().getMnemonic().type.equals(TokenType.XMNEMONIC_RL)) {
-				hazardPresent = (pipeline[1].getInstruction().getArgs()[0] == IDEX_Rd || 
+				hazardPresent = (pipeline[1].getInstruction().getArgs()[0] == XIDEX_Rd || 
 						pipeline[1].getInstruction().getArgs()[0] == EXMEM_Rd);
 			}
 		}
@@ -238,7 +322,8 @@ public class PipelinedSimulator extends LEGv8_Simulator {
 	// returns true if the specified instruction sets the flag values
 	private boolean setsFlags (Mnemonic m) {
 		return (m.equals(Mnemonic.ADDS) || m.equals(Mnemonic.SUBS) || m.equals(Mnemonic.ANDS) || 
-				m.equals(Mnemonic.ADDIS) || m.equals(Mnemonic.SUBIS) || m.equals(Mnemonic.ANDIS));
+				m.equals(Mnemonic.ADDIS) || m.equals(Mnemonic.SUBIS) || m.equals(Mnemonic.ANDIS) ||
+				m.equals(Mnemonic.FCMPD) || m.equals(Mnemonic.FCMPS) || m.equals(Mnemonic.CMP) || m.equals(Mnemonic.CMPI));
 	}
 	
 	// returns true if register dependency exists for load instruction in EX stage of pipeline
@@ -246,26 +331,26 @@ public class PipelinedSimulator extends LEGv8_Simulator {
 		boolean hazardPresent = false;
 		if (pipeline[2] != null) {
 			hazardPresent = pipeline[2].getInstruction().getControlSignals().equals(ControlUnitConfiguration.RM_LOAD) &&
-					(IDEX_Rd == IFID_Rn || IDEX_Rd == IFID_Rm);
+					(XIDEX_Rd == IFID_Rn || XIDEX_Rd == IFID_Rm);
 		}
 		return hazardPresent;
 	}
 	
 	// The logic conditions in the next four methods are defined in Hennessy and Patterson ARM edition pages 320, 322, and 324
 	private boolean dataHazardEXRn() {
-		return EXMEM_WB_RegWrite && EXMEM_Rd != CPU.XZR && EXMEM_Rd == IDEX_Rn;
+		return EXMEM_WB_RegWrite && EXMEM_Rd != CPU.XZR && EXMEM_Rd == XIDEX_Rn;
 	}
 	
 	private boolean dataHazardEXRm() {
-		return EXMEM_WB_RegWrite && EXMEM_Rd != CPU.XZR && EXMEM_Rd == IDEX_Rm;
+		return EXMEM_WB_RegWrite && EXMEM_Rd != CPU.XZR && EXMEM_Rd == XIDEX_Rm;
 	}
 	
 	private boolean dataHazardMEMRn() {
-		return MEMWB_WB_RegWrite && MEMWB_Rd != CPU.XZR && !(EXMEM_WB_RegWrite && EXMEM_Rd != CPU.XZR && EXMEM_Rd == IDEX_Rn) && MEMWB_Rd == IDEX_Rn;
+		return MEMWB_WB_RegWrite && MEMWB_Rd != CPU.XZR && !(EXMEM_WB_RegWrite && EXMEM_Rd != CPU.XZR && EXMEM_Rd == XIDEX_Rn) && MEMWB_Rd == XIDEX_Rn;
 	}
 	
 	private boolean dataHazardMEMRm() {
-		return MEMWB_WB_RegWrite && MEMWB_Rd != CPU.XZR && !(EXMEM_WB_RegWrite && EXMEM_Rd != CPU.XZR && EXMEM_Rd == IDEX_Rm) && MEMWB_Rd == IDEX_Rm;
+		return MEMWB_WB_RegWrite && MEMWB_Rd != CPU.XZR && !(EXMEM_WB_RegWrite && EXMEM_Rd != CPU.XZR && EXMEM_Rd == XIDEX_Rm) && MEMWB_Rd == XIDEX_Rm;
 	}
 	
 	// Updates the values of all the data held in the four pipeline registers
@@ -296,17 +381,17 @@ public class PipelinedSimulator extends LEGv8_Simulator {
 		IFID_Rn = -1;
 		IFID_Rm = -1;
 		IFID_Rd = -1;
-		IDEX_WB_RegWrite = false;
-		IDEX_WB_MemToReg = false;
-		IDEX_M_MemRead = false;
-		IDEX_M_MemWrite = false;
-		IDEX_EX_ALUOp = null;
-		IDEX_EX_ALUSrc = false;
-		IDEX_ReadData1 = null;
-		IDEX_ReadData2 = null;
-		IDEX_Rn = -1;
-		IDEX_Rm = -1;
-		IDEX_Rd = -1;
+		XIDEX_WB_RegWrite = false;
+		XIDEX_WB_MemToReg = false;
+		XIDEX_M_MemRead = false;
+		XIDEX_M_MemWrite = false;
+		XIDEX_EX_ALUOp = null;
+		XIDEX_EX_ALUSrc = false;
+		XIDEX_ReadData1 = null;
+		XIDEX_ReadData2 = null;
+		XIDEX_Rn = -1;
+		XIDEX_Rm = -1;
+		XIDEX_Rd = -1;
 		EXMEM_WB_RegWrite = false;
 		EXMEM_WB_MemToReg = false;
 		EXMEM_M_MemRead =  false;
@@ -364,51 +449,51 @@ public class PipelinedSimulator extends LEGv8_Simulator {
 	// Updates the values held in the IDEX pipeline register
 	private void updateIDEX() {
 		ControlUnitConfiguration c = pipeline[2].getInstruction().getControlSignals();
-		IDEX_WB_RegWrite = c.regWrite;
-		IDEX_WB_MemToReg = c.memToReg;
-		IDEX_M_MemRead = c.memRead;
-		IDEX_M_MemWrite = c.memWrite;
-		IDEX_EX_ALUOp = c.aluOp;
-		IDEX_EX_ALUSrc = c.aluSrc;
+		XIDEX_WB_RegWrite = c.regWrite;
+		XIDEX_WB_MemToReg = c.memToReg;
+		XIDEX_M_MemRead = c.memRead;
+		XIDEX_M_MemWrite = c.memWrite;
+		XIDEX_EX_ALUOp = c.aluOp;
+		XIDEX_EX_ALUSrc = c.aluSrc;
 		TokenType insGroup = pipeline[2].getInstruction().getMnemonic().type;
 		int[] a = pipeline[2].getInstruction().getArgs();
 		switch (insGroup) {
 		case XMNEMONIC_RRR :
-			IDEX_Rd = a[0];
-			IDEX_Rn = a[1];
-			IDEX_Rm = a[2];
-			IDEX_ReadData1 = pipeline[2].getSnapshotBefore().getRegister(a[1]);
-			IDEX_ReadData2 = pipeline[2].getSnapshotBefore().getRegister(a[2]);
+			XIDEX_Rd = a[0];
+			XIDEX_Rn = a[1];
+			XIDEX_Rm = a[2];
+			XIDEX_ReadData1 = pipeline[2].getSnapshotBefore().getRegister(RegisterType.X, a[1]);
+			XIDEX_ReadData2 = pipeline[2].getSnapshotBefore().getRegister(RegisterType.X, a[2]);
 			break;
 		case XMNEMONIC_RRI :
-			IDEX_Rd = a[0];
-			IDEX_Rn = a[1];
-			IDEX_ReadData1 = pipeline[2].getSnapshotBefore().getRegister(a[1]);
+			XIDEX_Rd = a[0];
+			XIDEX_Rn = a[1];
+			XIDEX_ReadData1 = pipeline[2].getSnapshotBefore().getRegister(RegisterType.X, a[1]);
 			break;
 		case XMNEMONIC_RM :
-			if (IDEX_WB_RegWrite) {
-				IDEX_Rd = a[0];
-				IDEX_Rn = a[1];
-				IDEX_ReadData1 = pipeline[2].getSnapshotBefore().getRegister(a[1])+a[2];
+			if (XIDEX_WB_RegWrite) {
+				XIDEX_Rd = a[0];
+				XIDEX_Rn = a[1];
+				XIDEX_ReadData1 = pipeline[2].getSnapshotBefore().getRegister(RegisterType.X, a[1])+a[2];
 			} else {
-				IDEX_Rn = a[1];
-				IDEX_Rm = a[0];
-				IDEX_ReadData1 = pipeline[2].getSnapshotBefore().getRegister(a[1]);
-				IDEX_ReadData2 = pipeline[2].getSnapshotBefore().getRegister(a[0]);
+				XIDEX_Rn = a[1];
+				XIDEX_Rm = a[0];
+				XIDEX_ReadData1 = pipeline[2].getSnapshotBefore().getRegister(RegisterType.X, a[1]);
+				XIDEX_ReadData2 = pipeline[2].getSnapshotBefore().getRegister(RegisterType.X, a[0]);
 			}
 			break;
 		case XMNEMONIC_RRM :
-			IDEX_Rd = a[1];
-			IDEX_Rn = a[2];
-			IDEX_Rm = a[0];
-			IDEX_ReadData1 = pipeline[2].getSnapshotBefore().getRegister(a[1]);
-			IDEX_ReadData2 = pipeline[2].getSnapshotBefore().getRegister(a[0]);
+			XIDEX_Rd = a[1];
+			XIDEX_Rn = a[2];
+			XIDEX_Rm = a[0];
+			XIDEX_ReadData1 = pipeline[2].getSnapshotBefore().getRegister(RegisterType.X, a[1]);
+			XIDEX_ReadData2 = pipeline[2].getSnapshotBefore().getRegister(RegisterType.X, a[0]);
 			break;
 		case XMNEMONIC_RISI :
-			IDEX_Rd = a[0];
+			XIDEX_Rd = a[0];
 			break;
 		case XMNEMONIC_RL :
-			IDEX_Rm = a[0];
+			XIDEX_Rm = a[0];
 			break;
 		default:
 			break;
@@ -427,28 +512,28 @@ public class PipelinedSimulator extends LEGv8_Simulator {
 		switch (insGroup) {
 		case XMNEMONIC_RRR :
 			EXMEM_Rd = a[0];
-			EXMEM_ALUResult = pipeline[3].getSnapshotAfter().getRegister(a[0]);
+			EXMEM_ALUResult = pipeline[3].getSnapshotAfter().getRegister(RegisterType.X, a[0]);
 			break;
 		case XMNEMONIC_RRI :
 			EXMEM_Rd = a[0];
-			EXMEM_ALUResult = pipeline[3].getSnapshotAfter().getRegister(a[0]);
+			EXMEM_ALUResult = pipeline[3].getSnapshotAfter().getRegister(RegisterType.X, a[0]);
 			break;
 		case XMNEMONIC_RM :
 			if (EXMEM_WB_RegWrite) {
 				EXMEM_Rd = a[0];
 			} else {
-				EXMEM_WriteDataMem = pipeline[3].getSnapshotBefore().getRegister(a[0]);
+				EXMEM_WriteDataMem = pipeline[3].getSnapshotBefore().getRegister(RegisterType.X, a[0]);
 			}
-			EXMEM_ALUResult = pipeline[3].getSnapshotBefore().getRegister(a[1])+a[2];
+			EXMEM_ALUResult = pipeline[3].getSnapshotBefore().getRegister(RegisterType.X, a[1])+a[2];
 			break;
 		case XMNEMONIC_RRM :
-			EXMEM_ALUResult = pipeline[3].getSnapshotBefore().getRegister(a[2])+a[3];
-			EXMEM_WriteDataMem = pipeline[3].getSnapshotBefore().getRegister(a[0]);
+			EXMEM_ALUResult = pipeline[3].getSnapshotBefore().getRegister(RegisterType.X, a[2])+a[3];
+			EXMEM_WriteDataMem = pipeline[3].getSnapshotBefore().getRegister(RegisterType.X, a[0]);
 			EXMEM_Rd = a[1];
 			break;
 		case XMNEMONIC_RISI :
 			EXMEM_Rd = a[0];
-			EXMEM_ALUResult = pipeline[3].getSnapshotAfter().getRegister(a[0]);
+			EXMEM_ALUResult = pipeline[3].getSnapshotAfter().getRegister(RegisterType.X, a[0]);
 			break;
 		default:
 			break;
@@ -465,27 +550,27 @@ public class PipelinedSimulator extends LEGv8_Simulator {
 		switch (insGroup) {
 		case XMNEMONIC_RRR :
 			MEMWB_Rd = a[0];
-			MEMWB_ALUResult = pipeline[4].getSnapshotAfter().getRegister(a[0]);
+			MEMWB_ALUResult = pipeline[4].getSnapshotAfter().getRegister(RegisterType.X, a[0]);
 			break;
 		case XMNEMONIC_RRI :
 			MEMWB_Rd = a[0];
-			MEMWB_ALUResult = pipeline[4].getSnapshotAfter().getRegister(a[0]);
+			MEMWB_ALUResult = pipeline[4].getSnapshotAfter().getRegister(RegisterType.X, a[0]);
 			break;
 		case XMNEMONIC_RM :
 			if (MEMWB_WB_RegWrite) {
 				MEMWB_Rd = a[0];
-				MEMWB_ReadDataMem = pipeline[4].getSnapshotAfter().getRegister(a[0]);
+				MEMWB_ReadDataMem = pipeline[4].getSnapshotAfter().getRegister(RegisterType.X, a[0]);
 			}
-			MEMWB_ALUResult = pipeline[4].getSnapshotBefore().getRegister(a[1])+a[2];
+			MEMWB_ALUResult = pipeline[4].getSnapshotBefore().getRegister(RegisterType.X, a[1])+a[2];
 			break;
 		case XMNEMONIC_RRM :
-			MEMWB_ALUResult = pipeline[4].getSnapshotBefore().getRegister(a[2])+a[3];
-			MEMWB_ReadDataMem = pipeline[4].getSnapshotAfter().getRegister(a[1]);
+			MEMWB_ALUResult = pipeline[4].getSnapshotBefore().getRegister(RegisterType.X, a[2])+a[3];
+			MEMWB_ReadDataMem = pipeline[4].getSnapshotAfter().getRegister(RegisterType.X, a[1]);
 			MEMWB_Rd = a[1];
 			break;
 		case XMNEMONIC_RISI :
 			MEMWB_Rd = a[0];
-			MEMWB_ALUResult = pipeline[4].getSnapshotAfter().getRegister(a[0]);
+			MEMWB_ALUResult = pipeline[4].getSnapshotAfter().getRegister(RegisterType.X, a[0]);
 			break;
 		default:
 			break;
@@ -494,7 +579,7 @@ public class PipelinedSimulator extends LEGv8_Simulator {
 	
 	@Override
 	public long getCPURegister(RegisterType type, int index) {
-		return visibleState.getRegister(index);
+		return visibleState.getRegister(type, index);
 	}
 	
 	@Override
@@ -538,7 +623,7 @@ public class PipelinedSimulator extends LEGv8_Simulator {
 	}
 	
 	private String logIDEX() {
-		return "IDEX_WB_RegWrite: " + IDEX_WB_RegWrite + ", IDEX_Rd: " + IDEX_Rd + ", IDEX_Rn: " + IDEX_Rn + ", IDEX_Rm: " + IDEX_Rm + "\n";
+		return "IDEX_WB_RegWrite: " + XIDEX_WB_RegWrite + ", IDEX_Rd: " + XIDEX_Rd + ", IDEX_Rn: " + XIDEX_Rn + ", IDEX_Rm: " + XIDEX_Rm + "\n";
 	}
 	
 	private String logEXMEM() {
@@ -561,61 +646,5 @@ public class PipelinedSimulator extends LEGv8_Simulator {
 		pipelineLog.append("\n");
 	}
 	
-	// All of the unused variables are control signal values - can be obtained from ControlUnitConfigureations class
-	// will be necessary when visualising the pipeline
 	
-	// It could be argued strongly that each pipeline register should be represented by an 
-	// object to make this a cleaner, more hierarchical structure!
-	
-	// values stored in IFID pipeline register
-	private long IFID_PC4 = -1;
-	private long IFID_Instruction = -1;
-	private int IFID_Rn = -1;
-	private int IFID_Rm = -1;
-	private int IFID_Rd = -1;
-	
-	// values stored in IDEX pipeline register
-	private boolean IDEX_WB_RegWrite = false;
-	private boolean IDEX_WB_MemToReg = false;
-	private boolean IDEX_M_MemRead = false;
-	private boolean IDEX_M_MemWrite = false;
-	private Integer IDEX_EX_ALUOp = null;
-	private boolean IDEX_EX_ALUSrc = false;
-	private Long IDEX_ReadData1 = null;
-	private Long IDEX_ReadData2 = null;
-	private int IDEX_Rn = -1;
-	private int IDEX_Rm = -1;
-	private int IDEX_Rd = -1;
-	
-	// values stored in the EXMEM pipeline register
-	private boolean EXMEM_WB_RegWrite = false;
-	private boolean EXMEM_WB_MemToReg = false;
-	private boolean EXMEM_M_MemRead =  false;
-	private boolean EXMEM_M_MemWrite = false;
-	private Long EXMEM_ALUResult = null;
-	private Long EXMEM_WriteDataMem = null;
-	private int EXMEM_Rd = -1;
-	
-	// values stored in the MEMWB pipeline refister
-	private boolean MEMWB_WB_RegWrite = false;
-	private boolean MEMWB_WB_MemToReg = false;
-	private Long MEMWB_ReadDataMem = null;
-	private Long MEMWB_ALUResult = null;
-	private int MEMWB_Rd = -1;
-	
-	// data hazard variables
-	private boolean dataHazardStall = false;
-	private boolean branchDataHazardStall = false;
-	private boolean EXHazard = false;
-	private boolean MEMHazard = false;
-	private boolean forwardA = false;
-	private boolean forwardB = false;
-	//control hazard variables
-	private boolean controlHazardStall = false;
-	private Instruction nextInstruction;
-	
-	private StringBuilder pipelineLog = new StringBuilder("");
-	private CPUSnapshot visibleState;
-	// array representing the pipeline
-	private PipelineInstruction[] pipeline = new PipelineInstruction[PIPELINE_SIZE];
 }
